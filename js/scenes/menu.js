a27a1ddb3004f7f433049449c7a88ffab3c43c6d
Kyle <kfalicov@gmail.com>
2019-05-01 01:51:59 -0400

    hero sprites
    no capes!

--- js/scenes/menu.js
+++ js/scenes/menu.js
@@ -58,8 +58,6 @@ class MainMenu extends Phaser.Scene {
 
         let background = this.add.sprite(0, 0, 'menu_background');
         background.setOrigin(0, 0);
-        
-        
 
         let bg = this.add.tileSprite(400,105,800,210, 'menu_white').setTint('0xff0000').setBlendMode(Phaser.BlendModes.ADD);
         let fg = this.add.tileSprite(400,105, 800, 128,'big_laser_fg').setBlendMode(Phaser.BlendModes.ADD);
